c30aab6eee906daaa1dcabebca3b3daaf516e217
josueeegl <josuegl12@outlook.com>
2022-08-08 22:31:20 -0600

    v2

--- api/controllers/info.js
+++ api/controllers/info.js
@@ -24,7 +24,7 @@ module.exports = {
             array.push({ title: property, data: [result[property]] });
           }
           const response = await fetch(
-            "http://localhost:3000/transacciones/resumen",
+            "http://localhost:3000/transacciones/resumen", 
             {
               method: "GET",
               headers: {
